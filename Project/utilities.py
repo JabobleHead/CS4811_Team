ac7f0b962c9c0d97171ceb477bcf835fcc5c1b95
Sickmannnnnn <zack.sickmann@icloud.com>
2026-03-18 16:05:44 -0400

    check_url works with wikipedia

--- Project/utilities.py
+++ Project/utilities.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
 
 def check_url(url):
         try:
-            response = requests.get(url)
+            response = requests.get(url, headers = headers)
             soup = BeautifulSoup(response.text, "html.parser")
-
-            print(url, " is a valid url")
-
-            return soup
-        
+            print(url, " is valid")
         except:
-            print(url, " is not a valid url")
-            return None
+             print(url, " is not valid")
+
+
+for url in ["https://beautiful-soup-4.readthedocs.io/en/latest/", "https://fakeurl.com", "https://en.wikipedia.org/wiki/Appellate_Division_Courthouse_of_New_York_State"]:
+    check_url(url)
